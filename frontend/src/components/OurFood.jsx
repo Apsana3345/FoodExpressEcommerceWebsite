fa20fc2cc9a9b7d19289978a5769eaee9889bae7
Apsana <apsana20580912@gmail.com>
2025-03-07 09:14:55 +0545

    commit

--- frontend/src/components/OurFood.jsx
+++ frontend/src/components/OurFood.jsx
@@ -1,43 +1,49 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import Title from './Title';
-import Wrapper from './layout/Wrapper';
-import ProductItems from './ProductItems';
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import Wrapper from "./layout/Wrapper";
+import ProductItems from "./ProductItems";
 
 const OurFood = () => {
+  const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
 
-const {products} = useContext(ShopContext);
-
-const [latestProducts,setLatestProducts]=useState([]);
-useEffect(()=>{
-setLatestProducts(products.slice(0,14));
-},[products])
-
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 14));
+  }, [products]);
 
   return (
-    <div className='my-10'>
-<Wrapper>
+    <div className="my-10 p-4">
+      <Wrapper>
+        <div className="text-center flex flex-col py-8 px-4">
+          <Title text1="Our" text2="Foods" />
+          <p className="w-3/4 text-base sm:text-sm md:text-lg text-left text-gray-700">
+            Experience the perfect blend of taste and quality with our delicious
+            food selection. From hearty meals to delightful treats, every dish
+            is crafted with the finest ingredients and rich flavors to satisfy
+            your cravings. Whether you're in the mood for something savory or
+            sweet, our menu offers a variety of mouthwatering options made with
+            love and passion. Indulge in fresh, flavorful, and wholesome
+            goodness with every bite!
+          </p>
+        </div>
 
-<div className="text-center flex flex-col  py-8 text-3xl p-4">
-   <Title text1={"Our "} text2={'Foods'} /> 
-<p className='w-3/4  text-base sm:text-sm md:text-lg  text-left text-gray-700 '>
-
-Experience the perfect blend of taste and quality with our delicious food selection. From hearty meals to delightful treats, every dish is crafted with the finest ingredients and rich flavors to satisfy your cravings. Whether you're in the mood for something savory or sweet, our menu offers a variety of mouthwatering options made with love and passion. Indulge in fresh, flavorful, and wholesome goodness with every bite!
-
-</p>
-</div>
-{/* Products rendering */}
-<div className='grid gridcols-2  sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  gap-4 gap-y-6 p-4'>
-{latestProducts.map((item,index)=>(
-<ProductItems key={index} id={item._id} image={item.image[0]} name={item.name} price={item.price}/>
-))}
-
-
-</div>
-</Wrapper>
-
+        <div className="mx-auto w-full px-4 sm:px-0">
+          <div className="grid grid-cols-2 max-[450px]:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+            {latestProducts.map((item, index) => (
+              <ProductItems
+                key={index}
+                id={item._id}
+                image={item.image[0]}
+                name={item.name}
+                price={item.price}
+              />
+            ))}
+          </div>
+        </div>
+      </Wrapper>
     </div>
-  )
-}
+  );
+};
 
-export default OurFood
+export default OurFood;
--- frontend/src/components/TodaysFood.jsx
+++ frontend/src/components/TodaysFood.jsx
@@ -7,6 +7,7 @@ import ProductItems from "./ProductItems";
 const TodaysFood = () => {
   const { products } = useContext(ShopContext);
   const [bestSeller, setBestSeller] = useState([]);
+
   useEffect(() => {
     const todayProduct = products.filter((item) => item.bestseller);
     setBestSeller(todayProduct.slice(0, 6));
@@ -15,8 +16,8 @@ const TodaysFood = () => {
   return (
     <div className="my-10 p-4">
       <Wrapper>
-        <div className="text-center flex flex-col  py-8 text-3xl p-4">
-          <Title text1={"Today's"} text2={"Food"} />
+        <div className="text-center flex flex-col py-8 px-4">
+          <Title text1="Today's" text2="Food" />
           <p className="w-3/4  text-base sm:text-sm md:text-lg  text-left text-gray-700 ">
             Indulge in a delicious selection of fresh and flavorful dishes,
             crafted with the finest ingredients. From wholesome meals to
@@ -26,16 +27,18 @@ const TodaysFood = () => {
           </p>
         </div>
 
-        <div className="grid gridcols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5   gap-6 gap-y-6 p-3">
-          {bestSeller.map((item, index) => (
-            <ProductItems
-              key={index}
-              id={item._id}
-              image={item.image[0]}
-              name={item.name}
-              price={item.price}
-            />
-          ))}
+        <div className="mx-auto w-full px-4 sm:px-0">
+          <div className="grid grid-cols-2 max-[450px]:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+            {bestSeller.map((item, index) => (
+              <ProductItems
+                key={index}
+                id={item._id}
+                image={item.image[0]}
+                name={item.name}
+                price={item.price}
+              />
+            ))}
+          </div>
         </div>
       </Wrapper>
     </div>
@@ -43,4 +46,3 @@ const TodaysFood = () => {
 };
 
 export default TodaysFood;
-
